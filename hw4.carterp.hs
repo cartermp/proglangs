b18b61ee65c5c342b78d2b627ec789f57ff40922
cartermp <phillip.carter@outlook.com>
2015-05-18 22:16:18 -0700

    first hw4 commit and finished cmd

---
+++ hw4.carterp.hs
@@ -0,0 +1,74 @@
+module HW4 where
+
+import MiniMiniLogo
+import Render
+
+
+--
+-- * Semantics of MiniMiniLogo
+--
+
+-- NOTE:
+--   * MiniMiniLogo.hs has the definitions of Mode, Cmd, and Prog!
+--   * Render.hs has the definitions of Point and Line!
+
+-- | A type to represent the current state of the pen.
+type State = (Mode,Point)
+
+-- | The initial state of the pen.
+start :: State
+start = (Up,(0,0))
+
+-- | A function that renders the image to HTML. Only works after you have
+--   implemented `prog`. Applying `draw` to a MiniMiniLogo program will
+--   produce an HTML file named MiniMiniLogo.html, which you can load in
+--   your browswer to view the rendered image.
+draw :: Prog -> IO ()
+draw p = let (_,ls) = prog p start in toHTML ls
+
+
+-- Semantic domains:
+--   * Cmd:  State -> (State, Maybe Line)
+--   * Prog: State -> (State, [Line])
+
+
+-- | Semantic function for Cmd.
+--
+--   >>> cmd (Pen Down) (Up,(2,3))
+--   ((Down,(2,3)),Nothing)
+--
+--   >>> cmd (Pen Up) (Down,(2,3))
+--   ((Up,(2,3)),Nothing)
+--
+--   >>> cmd (Move 4 5) (Up,(2,3))
+--   ((Up,(4,5)),Nothing)
+--
+--   >>> cmd (Move 4 5) (Down,(2,3))
+--   ((Down,(4,5)),Just ((2,3),(4,5)))
+--
+cmd :: Cmd -> State -> (State, Maybe Line)
+cmd c (m, (px, py)) = case (c, m) of
+               (Pen dir, _)  -> ((dir, (px, py)), Nothing)
+               (Move x y, Down) -> ((m, (x,y)), Just ((px,py), (x,y)))
+               (Move x y, Up) -> ((m, (x,y)), Nothing)
+
+
+-- | Semantic function for Prog.
+--
+--   >>> prog (nix 10 10 5 7) start
+--   ((Down,(15,10)),[((10,10),(15,17)),((10,17),(15,10))])
+--
+--   >>> prog (steps 2 0 0) start
+--   ((Down,(2,2)),[((0,0),(0,1)),((0,1),(1,1)),((1,1),(1,2)),((1,2),(2,2))])
+prog :: Prog -> State -> (State, [Line])
+prog = undefined
+
+
+--
+-- * Extra credit
+--
+
+-- | This should be a MiniMiniLogo program that draws an amazing picture.
+--   Add as many helper functions as you want.
+amazing :: Prog
+amazing = undefined
